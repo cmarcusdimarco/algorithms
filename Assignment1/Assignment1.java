b24837b2fc1624c1022210a124520712d564c187
cmarcusdimarco <C.DiMarco1@marist.edu>
2021-09-10 12:46:25 -0400

    Outlined classes and methods needed

---
+++ Assignment1/Assignment1.java
@@ -0,0 +1,35 @@
+/**
+ * Custom Node class which will serve as the container for individual
+ * characters from the strings to be tested. Must ignore spaces and
+ * capitalization, and must point to another Node object or null.
+ */
+class MarcusNode {
+    // Needs fields for item and pointer
+}
+
+/**
+ * Custom Stack class which will take Nodes as input and will output
+ * those Nodes in reverse order.
+ */
+class MarcusStack {
+    // Need methods for push, pop, isEmpty
+}
+
+/**
+ * Custom Queue class which will take Nodes as input and will output
+ * those Nodes in same order.
+ */
+class MarcusQueue {
+    // Need methods for enqueue, dequeue, isEmpty
+}
+
+/**
+ * A program which reads a String from a file and uses a Stack and Queue
+ * to compare it to its reverse order. If the Stack output and Queue output
+ * match, prints the String and increments a palindrome counter.
+ */
+class Assignment1 {
+    public static void main(String[] args) {
+    // Needs Scanner, source file, array of String objects
+    }
+}
