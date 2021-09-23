db01dd6ea3ca9d01a8cacf92a9825d8fd21da072
cmarcusdimarco <cmarcusdimarco@gmail.com>
2021-09-23 08:54:14 -0400

    Added java.io.File and java.util.Scanner.

--- Assignment1/Assignment1.java
+++ Assignment1/Assignment1.java
@@ -3,8 +3,18 @@
  * to compare it to its reverse order. If the Stack output and Queue output
  * match, prints the String and increments a palindrome counter.
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class Assignment1 {
     public static void main(String[] args) {
-    // Needs Scanner, source file, array of String objects
+        File magicItems = new File("magicitems.txt");
+        try {
+            Scanner read = new Scanner(magicItems);
+        } catch (FileNotFoundException e) {
+            System.out.println("Whoops! Couldn't find magicitems.txt");
+            e.printStackTrace();
+        }
     }
 }
