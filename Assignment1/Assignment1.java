45a9751dcccbac5e2c870dc30bc90134eb34abdd
cmarcusdimarco <cmarcusdimarco@gmail.com>
2021-09-24 12:26:27 -0400

    Added masterQueue for readability

--- Assignment1/Assignment1.java
+++ Assignment1/Assignment1.java
@@ -9,20 +9,28 @@ import java.util.Scanner;
 
 class Assignment1 {
     public static void main(String[] args) {
-        MarcusStack stack = new MarcusStack();
-        MarcusQueue queue = new MarcusQueue();
+        MarcusStack stack = new MarcusStack();          // Stack for testing Strings for palindromes
+        MarcusQueue queue = new MarcusQueue();          // Queue for testing Strings for palindromes
+        MarcusQueue masterQueue = new MarcusQueue();    // Queue to contain all Strings from File
 
+        // Try/catch block for file import and reading
         try {
             File file = new File("./Assignment1/magicitems.txt");
             Scanner read = new Scanner(file);
             while (read.hasNextLine()) {
                 MarcusNode item = new MarcusNode(read.nextLine());
-                queue.enqueue(item);
+                masterQueue.enqueue(item);
             }
             read.close();
         } catch (FileNotFoundException e) {
             System.out.println("Whoops! Couldn't find magicitems.txt");
             e.printStackTrace();
+        }
+
+        // For each item in masterQueue, force lowercase and remove extra characters
+        while (!masterQueue.isEmpty()) {
+            // Thinking about changing the Marcus classes to support Strings as well as Nodes
+            String string = masterQueue.dequeue().getItem();
         }
     }
 }
