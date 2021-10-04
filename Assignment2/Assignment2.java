0ab58a67a220403d9c37d01f1c5957244158aef0
cmarcusdimarco <C.DiMarco1@marist.edu>
2021-10-04 08:33:10 -0400

    Added outline of files

---
+++ Assignment2/Assignment2.java
@@ -0,0 +1,12 @@
+/**
+ * A program which reads a constant number of Strings
+ * from a file and sorts them using various methods,
+ * comparing the efficiency of the sorts by
+ * printing the number of comparisons for each.
+ */
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Assignment2/MarcusSort.java
@@ -0,0 +1,22 @@
+/**
+ * Custom Sort library including selection sort, insertion sort,
+ * merge sort and quick sort.
+ */
+
+public class MarcusSort {
+    public void selectionSort() {
+
+    }
+
+    public void insertionSort() {
+
+    }
+
+    public void mergeSort() {
+
+    }
+
+    public void quickSort() {
+
+    }
+}
