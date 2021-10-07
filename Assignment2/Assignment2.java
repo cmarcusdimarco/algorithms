0b216b518ae443ccd67d8c62cff958b2795afffd
cmarcusdimarco <C.DiMarco1@marist.edu>
2021-10-07 09:55:01 -0400

    Added try/catch file handling, program outline

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -4,9 +4,36 @@
  * comparing the efficiency of the sorts by
  * printing the number of comparisons for each.
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Assignment2 {
     public static void main(String[] args) {
-        
+        final int NUM_OF_ITEMS = 666;
+        String[] magicItems = new String[NUM_OF_ITEMS];
+
+        // Try/catch block for file import and reading
+        try {
+            File file = new File("magicitems.txt");
+            Scanner read = new Scanner(file);
+            for (int i = 0; i < NUM_OF_ITEMS; i++) {
+                magicItems[i] = read.nextLine();
+            }
+            read.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Whoops! Couldn't find magicitems.txt");
+            e.printStackTrace();
+        }
+
+        // Shuffle array before beginning sorts
+
+        // Sort using selection sort, print comparisons
+
+        // Sort using insertion sort, print comparisons
+
+        // Sort using merge sort, print comparisons
+
+        // Sort using quicksort, print comparisons
     }
 }
