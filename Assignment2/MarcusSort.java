4125745a5124c2d9fa638149947a7ac348c57b85
cmarcusdimarco <C.DiMarco1@marist.edu>
2021-10-07 10:08:39 -0400

    Added selection sort

--- Assignment2/MarcusSort.java
+++ Assignment2/MarcusSort.java
@@ -4,8 +4,26 @@
  */
 
 public class MarcusSort {
-    public void selectionSort() {
-
+    
+    // Selection sort will search for minimum unsorted value and
+    // place it at first unsorted index.
+    public String[] selectionSort(String[] strings) {
+        
+        // First unsorted index through each pass is i
+        for (int i = 0; i < strings.length; i++) {
+            int minIndex = i;
+            // Find index of smallest value in remainder of array
+            for (int j = i + 1; j < strings.length; j++) {
+                if (strings[minIndex].compareToIgnoreCase(strings[j]) > 0) {
+                    minIndex = j;
+                }
+            }
+            // Swap indices minIndex and i
+            String temp = strings[i];
+            strings[i] = strings[minIndex];
+            strings[minIndex] = temp;
+        }
+        return strings;
     }
 
     public void insertionSort() {
