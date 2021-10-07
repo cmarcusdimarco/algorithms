078203bdca56ed8a83d9e43ea5bf181e0094aee3
cmarcusdimarco <C.DiMarco1@marist.edu>
2021-10-07 10:21:30 -0400

    Added counter and counter functionality

--- Assignment2/MarcusSort.java
+++ Assignment2/MarcusSort.java
@@ -5,15 +5,21 @@
 
 public class MarcusSort {
     
+    private int counter = 0;    // Count number of comparisons
+
     // Selection sort will search for minimum unsorted value and
     // place it at first unsorted index.
     public String[] selectionSort(String[] strings) {
-        
+
+        // Start with counter at 0
+        this.resetCounter();
+
         // First unsorted index through each pass is i
         for (int i = 0; i < strings.length; i++) {
             int minIndex = i;
             // Find index of smallest value in remainder of array
             for (int j = i + 1; j < strings.length; j++) {
+                counter++;  // Increment for each comparison
                 if (strings[minIndex].compareToIgnoreCase(strings[j]) > 0) {
                     minIndex = j;
                 }
@@ -23,6 +29,8 @@ public class MarcusSort {
             strings[i] = strings[minIndex];
             strings[minIndex] = temp;
         }
+        // Print completion message with number of comparisons
+        this.printCompletionMessage();
         return strings;
     }
 
@@ -36,5 +44,20 @@ public class MarcusSort {
 
     public void quickSort() {
 
+    }
+
+    // Getter for counter
+    public int getCounter() {
+        return counter;
+    }
+
+    // Controlling setter functionality to only reset to 0
+    public void resetCounter() {
+        counter = 0; 
+    }
+
+    // Message to print upon completing sort which includes counter
+    public void printCompletionMessage() {
+        System.out.println("Sort complete! Number of comparisons: " + counter);
     }
 }
