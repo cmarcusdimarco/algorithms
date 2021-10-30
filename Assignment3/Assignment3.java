95252e10f9da47c2232494ab73905555d1551c37
cmarcusdimarco <C.DiMarco1@marist.edu>
2021-10-30 13:08:23 -0400

    Added assignment template from Assignment 2

---
+++ Assignment3/Assignment3.java
@@ -0,0 +1,30 @@
+/**
+ * A program which reads a constant number of Strings
+ * from a file and sorts them using various methods,
+ * comparing the efficiency of the sorts by
+ * printing the number of comparisons for each.
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Assignment3 {
+    public static void main(String[] args) {
+        final int NUM_OF_ITEMS = 666;                   // Length of file as constant
+        String[] magicItems = new String[NUM_OF_ITEMS]; // Array of file strings
+        MarcusSort sorter = new MarcusSort();           // Instance of MarcusSort
+
+        // Try/catch block for file import and reading
+        try {
+            File file = new File("magicitems.txt");
+            Scanner read = new Scanner(file);
+            for (int i = 0; i < NUM_OF_ITEMS; i++) {
+                magicItems[i] = read.nextLine();
+            }
+            read.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Whoops! Couldn't find magicitems.txt");
+            e.printStackTrace();
+        }
+    }
+}
