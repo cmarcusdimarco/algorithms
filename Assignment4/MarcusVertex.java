85e7185b8a2f7ab5192aec8e310204acabbfa6ca
cmarcusdimarco <C.DiMarco1@marist.edu>
2021-11-20 00:47:51 -0500

    Created Vertex class

---
+++ Assignment4/MarcusVertex.java
@@ -0,0 +1,44 @@
+/**
+ * A custom implementation of a vertex object for
+ * representing graphs as linked objects.
+ */
+import java.util.ArrayList;
+
+public class MarcusVertex {
+    private int id;
+    private boolean isProcessed;
+    private ArrayList<MarcusVertex> neighbors;
+
+    public MarcusVertex(int id) {
+        this.id = id;
+        this.isProcessed = false;
+        this.neighbors = new ArrayList<MarcusVertex>();
+    }
+
+    // Setters and getters for private fields
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean getIsProcessed() {
+        return isProcessed;
+    }
+
+    public void setIsProcessed(boolean isProcessed) {
+        this.isProcessed = isProcessed;
+    }
+
+    public void addNeighbor(MarcusVertex neighbor) {
+        this.neighbors.add(neighbor);
+    }
+
+    public void printNeighbors() {
+        for (MarcusVertex currentVertex : neighbors) {
+            System.out.print(currentVertex.getId() + " ");
+        }
+    }
+}
