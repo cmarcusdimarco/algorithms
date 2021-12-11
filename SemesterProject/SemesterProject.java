9363813d50309880cd61edda9813d40d2d9886cd
cmarcusdimarco <cmarcusdimarco@gmail.com>
2021-12-11 17:37:58 -0500

    Added constants, static shuffle method

--- SemesterProject/SemesterProject.java
+++ SemesterProject/SemesterProject.java
@@ -9,14 +9,38 @@
 // Import necessary libraries
 
 public class SemesterProject {
+    // A modified implementation of notRosannaShuffle() to shuffle
+    // an array of integers instead of Strings
+    public static void notRosannaShuffle(int[] integers) {
+
+        for (int i = integers.length - 1; i >= 0; i--) {
+            // Get random index in the remaining length
+            int randomIndex = (int) Math.floor(Math.random() * i);
+            if (randomIndex == i) {
+                continue;
+            } else {
+                // If the random index is not i, swap their values
+                String tempInt = integers[i];
+                integers[i] = integers[randomIndex];
+                integers[randomIndex] = tempInt;
+            }
+        }
+    }
     public static void main(String[] args) {
         // Initialize primary variables/data structures
+        final int POPULATION = Integer.parseInt(args[0]);
+        final double INFECTION_RATE = 0.02;
             // Population size as a constant
+        int[] populationArray = new int[POPULATION];
             // Array of population size
         
         // Infect 2% of array
+        for (int i = 0; i < (POPULATION * INFECTION_RATE); i++) {
+            populationArray[i] = 1;
+        }
 
         // Shuffle array to represent random distribution
+        notRosannaShuffle(populationArray);
 
         // Implement testing of subarrays of size 8
             // Case 1: sum of subarrays is 0
